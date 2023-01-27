3466a4caf828662b6abc4ceeafbe991cab846431
jameschapman19 <james.chapman.19@ucl.ac.uk>
2023-01-27 16:15:49 +0000

    Initial

--- skprox/linear_model/_base.py
+++ skprox/linear_model/_base.py
@@ -158,28 +158,3 @@ class RegularisedRegression(LinearRegression):
                 "delta": self.delta,
             }
         return _proximal_operators(self.proximal, **params)
-
-
-if __name__ == "__main__":
-    from sklearn.datasets import make_regression
-    from sklearn.linear_model import LinearRegression
-    from sklearn.model_selection import train_test_split
-
-    X, y, t = make_regression(
-        n_samples=100,
-        n_features=200,
-        n_informative=10,
-        n_targets=5,
-        random_state=1,
-        coef=True,
-    )
-    x_train, x_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=0
-    )
-    reg = RegularisedRegression(proximal="L21", sigma=0.1, max_iter=10000)
-    reg.fit(x_train, y_train)
-    print(reg.score(x_test, y_test))
-    lreg = LinearRegression()
-    lreg.fit(x_train, y_train)
-    print(lreg.score(x_test, y_test))
-    print()
