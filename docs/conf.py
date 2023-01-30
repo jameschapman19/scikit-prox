cdfeb80846b6d34b307768fe846cce50a975965d
jameschapman19 <james.chapman.19@ucl.ac.uk>
2023-01-30 11:55:22 +0000

    Initial

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,12 @@
 # Configuration file for the Sphinx documentation builder.
 #
+import os
+import sys
+import warnings
+import datetime
+sys.path.insert(0, os.path.abspath("../.."))
+
+
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
