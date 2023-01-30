3ee6f62952cce753dcc0ce80d6a8b8b5837b114e
jameschapman19 <james.chapman.19@ucl.ac.uk>
2023-01-30 11:38:25 +0000

    Initial

--- docs/conf.py
+++ docs/conf.py
@@ -14,9 +14,9 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['autoapi.extension']
-autoapi_add_toctree_entry = False
-autoapi_dirs = ['../skprox']
+#extensions = ['autoapi.extension']
+#autoapi_add_toctree_entry = False
+#autoapi_dirs = ['../skprox']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -26,5 +26,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
