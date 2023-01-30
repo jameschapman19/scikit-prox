f098e9140985ee1e2e2be435f33022abe291e59a
jameschapman19 <james.chapman.19@ucl.ac.uk>
2023-01-30 12:03:28 +0000

    Initial

--- docs/conf.py
+++ docs/conf.py
@@ -21,9 +21,9 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-#extensions = ['autoapi.extension']
-#autoapi_add_toctree_entry = False
-#autoapi_dirs = ['../skprox']
+extensions = ['autoapi.extension']
+autoapi_add_toctree_entry = False
+autoapi_dirs = ['../skprox']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
